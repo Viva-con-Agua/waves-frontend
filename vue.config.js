bb305f865a71e7d5d20ed08390f20e062e4c09af
Dennis Kleber <dennis_kleber@mailbox.org>
2020-01-09 15:40:19 +0100

    add vue.config.js and docker-compose.yml

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@ module.exports = {
     chainWebpack: config => {
         config.module
             .rule("vue")
-            .use("vue-svg-inline-loader")
-            .loader("vue-svg-inline-loader")
             .options({ /* ... */ });
     },
     baseUrl: process.env.NODE_ENV === 'production'
