e8f29b39f867382df528de9858caefc07aa8f9b1
Dennis Kleber <dennis_kleber@mailbox.org>
2020-01-25 20:21:02 +0100

    remove options function

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
     chainWebpack: config => {
         config.module
             .rule("vue")
-            .options({ /* ... */ });
     },
     baseUrl: process.env.NODE_ENV === 'production'
         ? '/waves/'
